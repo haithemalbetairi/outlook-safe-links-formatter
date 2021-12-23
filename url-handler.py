9a20d1eaefe1afd1a095389fe07693a1acba0199
Haithem Albetairi <haithem.t.albetairi@gmail.com>
2021-12-23 11:29:59 +0300

    Adding main src files
    Added the main python script and two "blank" (they have a space character) to the project. One is where the safe links will be pasted. The other, will have the properly formatted URLs to be used in scanning.

---
+++ url-handler.py
@@ -0,0 +1,37 @@
+# The logic behind this is code is to fix copied URLs from outllok in order to
+# scan them using VirusTotal.com
+# 
+# How to use:
+# Right-click an outlook link, copy it, paste it in safe-urls.txt, repeat for any links to test.
+# Put each in a new line.
+#
+# Resulting URLs are found in unsafe-urls.txt. Make sure not to click them in case they are malicious.
+#
+# Code flow:
+# Read the contents of the file safe-urls.txt, remove the Outlook safe link, 
+# replace the symbols with their correct URL indentified.
+#
+
+
+# Read the contents of the file safe-urls.txt
+oldURL = open('safe-urls.txt', 'r')
+oldURL = oldURL.read()
+
+# Remove the Outlook safe link
+oldURL = oldURL.replace('https://eur02.safelinks.protection.outlook.com/?url=', '')
+
+
+# The symbols to replace
+oldSymbols = ['%20', '%25', '%26', '%2B', '%2F', '%3A', '%3F', '%3D', '%40', '%7C',]
+newSymbols = [' ',   '%',   '&',   '+',   '/',   ':',   '?',   '=',   '@',   '|']
+
+
+# Loop replacing the symbols
+for old, new in zip(oldSymbols, newSymbols):
+    oldURL = oldURL.replace(old, new)
+
+# Write the modifyied URLs into unsafe-urls.txt
+unsafeURL = open('unsafe-urls.txt', 'a')
+unsafeURL.write(oldURL)
+unsafeURL.write('\n----------------------------------------------\n')
+unsafeURL.close()
